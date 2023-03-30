d43b54ac0d60d5408da1d2e4197b4509b9adc5df
PaulWaller <paul.waller@endjin.com>
2023-03-30 13:25:30 +0100

    Initial commit - added details and logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -65,6 +65,7 @@ export default function Layout ({ children, mainClassName, metaTitle, whiteBg, h
       </div>
 
       <Footer />
+      <OpenJS />
     </div>
   )
 }
@@ -295,6 +296,19 @@ const Footer = () => (
       <a href='/understanding-json-schema/credits' className='text-sm text-slate-400 hover:text-slate-500 pt-3'>Acknowledgments</a>
     </div>
   </footer>
+)
+
+const OpenJS = () => (
+  <div className={classnames(responsiveClasses, 'mx-auto p-4 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 ')}>
+    <div className='sm:grid-cols-1 lg:grid-cols-1'>
+      <div className='flex flex-row-reverse'>
+        <img className='h-16 mb-3.5' src="/img/logos/openjs_foundation-logo-horizontal-color.svg" alt="color openjs foundation logo"></img></div>
+      <div className="prose prose-a:text-blue-600 hover:prose-a:text-blue-500">
+        <p>Copyright <a href="https://openjsf.org">OpenJS Foundation</a> and JSON Schema contributors. All rights reserved. The <a href="https://openjsf.org">OpenJS Foundation</a> has registered trademarks and uses trademarks.  For a list of trademarks of the <a href="https://openjsf.org">OpenJS Foundation</a>, please see our <a href="https://trademark-policy.openjsf.org">Trademark Policy</a> and <a href="https://trademark-list.openjsf.org">Trademark List</a>.  Trademarks and logos not indicated on the <a href="https://trademark-list.openjsf.org">list of OpenJS Foundation trademarks</a> are trademarks&trade; or registered&reg; trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them.</p>
+        <p><a href="https://openjsf.org">The OpenJS Foundation</a> | <a href="https://terms-of-use.openjsf.org">Terms of Use</a> | <a href="https://privacy-policy.openjsf.org">Privacy Policy</a> | <a href="https://bylaws.openjsf.org">Bylaws</a> | <a href="https://code-of-conduct.openjsf.org">Code of Conduct</a> | <a href="https://trademark-policy.openjsf.org">Trademark Policy</a> | <a href="https://trademark-list.openjsf.org">Trademark List</a> | <a href="https://www.linuxfoundation.org/cookies">Cookie Policy</a></p>
+      </div>
+    </div>
+  </div>
 )
 
 const Logo = () => (
