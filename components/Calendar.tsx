faade5a483c997e8ecd8d7dd2e39f14f05d72bc9
OlliesWorld <rtlockwoodwork@gmail.com>
2023-06-07 18:05:41 -0600

    updated moment with z for zone

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -11,26 +11,27 @@ export default function Calendar() {
 
   const sortedEvents = eventsData.sort((a, b) => (a.date.toLowerCase() < b.date.toLowerCase()) ? -1 : ((b.date.toLowerCase() > a.date.toLowerCase()) ? 1 : 0))
 
-  
   return (
     <div>
       <Headline3>Upcoming Events</Headline3>
+      {/* {meetingsWithDates} */}
       <ul>
         {sortedEvents.slice(0, 3).map((event, index) => (
           <li key={index}>
             <div className='flex mb-4'>
               <p className='bg-btnOrange rounded-full w-10 h-10 p-2 text-center text-white mr-2'>
                 {moment(event.date).get('date')}
-              </p>
+              </p> 
               <div>
                 <p className=''>{event.title}</p>
                 
-                {moment(event.date).format('lll')}
+                {moment(event.date).utc().format('Do MMMM YYYY, h:mm a z')}
               
               </div>
             </div>
           </li>
         ))}
+
       </ul>
     </div>
   )
