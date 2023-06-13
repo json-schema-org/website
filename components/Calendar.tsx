53efdbeecc7b1b8a78a2614143e28074501949e5
OlliesWorld <rtlockwoodwork@gmail.com>
2023-06-13 09:08:19 -0600

    take out timezone specific

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import eventsData from './datesCalendar.json'
 import 'moment-timezone'
-// import moment from 'moment'
+import moment from 'moment'
 import Moment from 'react-moment'
 
 
@@ -13,19 +13,20 @@ export default function Calendar() {
   // const tzName = moment.tz.guess()
   return (
     <div>
-     
+
       <ul>
         {sortedEvents.slice(0, 3).map((event, index) => (
           <li key={index}>
             <div className='flex mb-4'>
               <p className='bg-btnOrange rounded-full w-10 h-10 p-2 text-center text-white mr-2'>
-                {/* {moment(event.date).get('date')} */}
-              </p> 
+                {moment(event.date).get('date')}
+              </p>
               <div>
                 <p className=''>{event.title}</p>
-                <Moment className='mb-0 date' tz='America/Los_Angeles' format='MM/DD/YYYY h:mma z'>{event.date}</Moment>
+                <Moment className='mb-0 date' format='D MMM YYYY h:mm z' utc
+                  local>{event.date}</Moment>
                 {/* {moment.tz(event.date, tzName).format('Do MMMM YYYY, HH:mm z')} */}
-              
+
               </div>
             </div>
           </li>
