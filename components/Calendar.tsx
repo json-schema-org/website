8ea583b1d519bb3e75fd01f86dc3b52a2951da3c
OlliesWorld <rtlockwoodwork@gmail.com>
2023-06-13 08:50:41 -0600

    added React-moment, fix ts.config, added blog hero and tags

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import eventsData from './datesCalendar.json'
 import 'moment-timezone'
-import moment from 'moment'
+// import moment from 'moment'
+import Moment from 'react-moment'
 
 
 export default function Calendar() {
@@ -9,7 +10,7 @@ export default function Calendar() {
   //     'https://calendar.google.com/calendar/u/0/embed?src=c_8r4g9r3etmrmt83fm2gljbatos@group.calendar.google.com';
 
   const sortedEvents = eventsData.sort((a, b) => (a.date.toLowerCase() < b.date.toLowerCase()) ? -1 : ((b.date.toLowerCase() > a.date.toLowerCase()) ? 1 : 0))
-  const tzName = moment.tz.guess()
+  // const tzName = moment.tz.guess()
   return (
     <div>
      
@@ -18,12 +19,12 @@ export default function Calendar() {
           <li key={index}>
             <div className='flex mb-4'>
               <p className='bg-btnOrange rounded-full w-10 h-10 p-2 text-center text-white mr-2'>
-                {moment(event.date).get('date')}
+                {/* {moment(event.date).get('date')} */}
               </p> 
               <div>
                 <p className=''>{event.title}</p>
-                
-                {moment.tz(event.date, tzName).format('Do MMMM YYYY, HH:mm z')}
+                <Moment className='mb-0 date' tz='America/Los_Angeles' format='MM/DD/YYYY h:mma z'>{event.date}</Moment>
+                {/* {moment.tz(event.date, tzName).format('Do MMMM YYYY, HH:mm z')} */}
               
               </div>
             </div>
--- pages/blog/index.page.tsx
+++ pages/blog/index.page.tsx
@@ -37,49 +37,89 @@ export async function getStaticProps() {
   }
 }
 
-export default function StaticMarkdownPage ({ blogPosts }: { blogPosts: any[] }) {
+export default function StaticMarkdownPage({ blogPosts }: { blogPosts: any[] }) {
   const router = useRouter()
   const typeFilter: null | string = Array.isArray(router.query?.type)
     ? router.query?.type?.[0] : router.query?.type || null
-
+  console.log('start', typeFilter)
   const setParam = useSetUrlParam()
 
+
+  const recentBlog = blogPosts.sort((a, b) => {
+    const dateA = new Date(a.frontmatter.date).getTime()
+    const dateB = new Date(b.frontmatter.date).getTime()
+    return dateA < dateB ? 1 : -1
+  })
+
+  const timeToRead = Math.ceil(readingTime(recentBlog[0].content).minutes)
+  const setOfTags: any[] = blogPosts.map((tag) => tag.frontmatter.type)
+  const spreadTags: any[] = [...setOfTags]
+  const allTags: any[] = [...new Set(spreadTags)]
+
+  // console.log(allTags)
   return (
     <SectionContext.Provider value='blog'>
       <Layout>
         <Head>
           <title>JSON Schema Blog</title>
         </Head>
-        <div className='bg-slate-100 mt-12'>
-          <div className='flex flex-row justify-between items-center bg-white xl:w-[1200px] mx-auto px-2 sm:px-4 lg:px-8'>
-            <Headline1>Blog</Headline1>
-            <a
-              href='/rss/feed.xml'
-              className='flex flex-row items-center text-blue-500 hover:text-blue-600 cursor-pointer mt-3'
-            >
-              <img src='/icons/rss.svg' className='rounded h-5 w-5 mr-2' />
-            RSS Feed
-            </a>
-          </div>
+        <div className='flex flex-col items-center mt-24'>
 
-          {typeFilter && (
-            <div className='flex flex-row'>
-              <svg className='h-4 w-4 fill-slate-300 mt-1.5 mr-2' height='394pt' viewBox='-5 0 394 394.00003' width='394pt' xmlns='http://www.w3.org/2000/svg'><path d='m367.820312 0h-351.261718c-6.199219-.0117188-11.878906 3.449219-14.710938 8.960938-2.871094 5.585937-2.367187 12.3125 1.300782 17.414062l128.6875 181.285156c.042968.0625.089843.121094.132812.183594 4.675781 6.3125 7.207031 13.960938 7.21875 21.816406v147.800782c-.027344 4.375 1.691406 8.582031 4.773438 11.6875 3.085937 3.101562 7.28125 4.851562 11.65625 4.851562 2.222656-.003906 4.425781-.445312 6.480468-1.300781l72.3125-27.570313c6.476563-1.980468 10.777344-8.09375 10.777344-15.453125v-120.015625c.011719-7.855468 2.542969-15.503906 7.214844-21.816406.042968-.0625.089844-.121094.132812-.183594l128.691406-181.289062c3.667969-5.097656 4.171876-11.820313 1.300782-17.40625-2.828125-5.515625-8.511719-8.9765628-14.707032-8.964844zm0 0' /></svg>
-              <div
-                className='inline-flex flex-row items-center bg-blue-100 hover:bg-blue-200 cursor-pointer font-semibold text-blue-800 inline-block px-3 py-1 rounded-full mb-4 text-sm'
-                onClick={() => setParam('type', null)}
+
+          {recentBlog[0] && (
+            <section className=' w-full  clip-bottom'>
+              <Link href={`/blog/posts/${recentBlog[0].slug}`} passHref>
+                <div>
+                  <div className='absolute z-20 left-10 text-white mt-72'>
+                    <div
+                      className='bg-blue-100 hover:bg-blue-200 cursor-pointer font-semibold text-blue-800 inline-block px-3 py-1 rounded-full mb-4 text-sm'
+                      onClick={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        setParam('type', recentBlog[0].frontmatter.type)
+                      }}
+                    >
+                      {recentBlog[0].frontmatter.type}
+                    </div>
+                    <div className='text-h1mobile md:text-h1 font-semibold'>
+                      {recentBlog[0].frontmatter.title}
+                    </div>
+                    <div className='flex ml-2 mb-2 '>
+                      <div className='bg-slate-50 h-[44px] w-[44px] rounded-full -ml-3 bg-cover bg-center border-2 border-white'
+                        style={{ backgroundImage: `url(${recentBlog[0].frontmatter.authors[0].photo})` }}
+                      />
+                      <div className='flex flex-col ml-2'>
+                        <p className='text-sm font-semibold'>{recentBlog[0].frontmatter.authors[0].name}</p>
+                        <div className=' text-sm'>
+                          <span>
+                            {recentBlog[0].frontmatter.date} &middot; {timeToRead} min read
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <img src={recentBlog[0].frontmatter.cover} className='relative z-10 bg-slate-50 w-full h-[595px] bg-cover bg-center' />
+                </div>
+              </Link>
+            </section>)}
+          <div className='w-full flex justify-between items-center mx-auto px-2 sm:px-4 lg:px-8'>
+            <div className='flex items-center'><Headline1>Blog</Headline1> <p className='mt-4'>Filter blog post by category...</p></div>
+            <div className='col-span-1'>
+              <a
+                href='/rss/feed.xml'
+                className='flex flex-row items-center text-blue-500 hover:text-blue-600 cursor-pointer mt-3'
               >
-                {typeFilter}
-                <svg
-                  className='fill-blue-800 h-2 w-2 ml-2'
-                  height='365.696pt' viewBox='0 0 365.696 365.696' width='365.696pt' xmlns='http://www.w3.org/2000/svg'>
-                  <path d='m243.1875 182.859375 113.132812-113.132813c12.5-12.5 12.5-32.765624 0-45.246093l-15.082031-15.082031c-12.503906-12.503907-32.769531-12.503907-45.25 0l-113.128906 113.128906-113.132813-113.152344c-12.5-12.5-32.765624-12.5-45.246093 0l-15.105469 15.082031c-12.5 12.503907-12.5 32.769531 0 45.25l113.152344 113.152344-113.128906 113.128906c-12.503907 12.503907-12.503907 32.769531 0 45.25l15.082031 15.082031c12.5 12.5 32.765625 12.5 45.246093 0l113.132813-113.132812 113.128906 113.132812c12.503907 12.5 32.769531 12.5 45.25 0l15.082031-15.082031c12.5-12.503906 12.5-32.769531 0-45.25zm0 0' />
-                </svg>
-              </div>
+                <img src='/icons/rss.svg' className='rounded h-5 w-5 mr-2' />
+                RSS Feed
+              </a>
             </div>
-          )}
+          </div>
+          <div className='flex justify-start lg:-ml-[1000px]'>{allTags.map((tag) => (
+            <p key={tag} className='bg-blue-100 hover:bg-blue-200 cursor-pointer font-semibold text-blue-800 inline-block px-3 py-1 rounded-full mb-4 text-sm'>{tag}</p>
+          ))}</div>
 
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 grid-flow-row mb-20 bg-white xl:w-[1200px] mx-auto px-2 sm:px-4 lg:px-8'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 grid-flow-row mb-20 bg-white  mx-auto px-2 sm:px-4 lg:px-8'>
             {blogPosts
               .filter(post => {
                 if (!typeFilter) return true
@@ -98,69 +138,72 @@ export default function StaticMarkdownPage ({ blogPosts }: { blogPosts: any[] })
                 const timeToRead = Math.ceil(readingTime(content).minutes)
 
                 return (
-                  <div
-                    key={blogPost.slug}
-                    className='flex border rounded-lg shadow-sm hover:shadow-lg transition-all overflow-hidden'
-                  >
-                    <Link href={`/blog/posts/${blogPost.slug}`}>
-                      <a className='inline-flex flex-col flex-1 w-full'>
-                        <div
-                          className='bg-slate-50 h-[160px] w-full self-stretch mr-3 bg-cover bg-center'
-                          style={{ backgroundImage: `url(${frontmatter.cover})` }}
-                        />
-                        <div className='p-4 flex flex-col flex-1 justify-between'>
-                          <div>
+                  <>
+
+                    <div
+                      key={blogPost.slug}
+                      className='flex border rounded-lg shadow-sm hover:shadow-lg transition-all overflow-hidden'
+                    >
+                      <Link href={`/blog/posts/${blogPost.slug}`}>
+                        <a className='inline-flex flex-col flex-1 w-full'>
+                          <div
+                            className='bg-slate-50 h-[160px] w-full self-stretch mr-3 bg-cover bg-center'
+                            style={{ backgroundImage: `url(${frontmatter.cover})` }}
+                          />
+                          <div className='p-4 flex flex-col flex-1 justify-between'>
                             <div>
-                              <div
-                                className='bg-blue-100 hover:bg-blue-200 cursor-pointer font-semibold text-blue-800 inline-block px-3 py-1 rounded-full mb-4 text-sm'
-                                onClick={(e) => {
-                                  e.preventDefault()
-                                  e.stopPropagation()
-                                  setParam('type', frontmatter.type)
-                                }}
-                              >
-                                {frontmatter.type}
+                              <div>
+                                <div
+                                  className='bg-blue-100 hover:bg-blue-200 cursor-pointer font-semibold text-blue-800 inline-block px-3 py-1 rounded-full mb-4 text-sm'
+                                  onClick={(e) => {
+                                    e.preventDefault()
+                                    e.stopPropagation()
+                                    setParam('type', frontmatter.type)
+                                  }}
+                                >
+                                  {frontmatter.type}
+                                </div>
+                              </div>
+                              <div className='text-lg font-semibold'>
+                                {frontmatter.title}
+                              </div>
+                              <div className='mt-3 mb-6 text-slate-500'>
+                                <TextTruncate element='span' line={4} text={frontmatter.excerpt} />
                               </div>
                             </div>
-                            <div className='text-lg font-semibold'>
-                              {frontmatter.title}
-                            </div>
-                            <div className='mt-3 mb-6 text-slate-500'>
-                              <TextTruncate element='span' line={4} text={frontmatter.excerpt} />
+                            <div className='flex flex-row items-center'>
+                              <div className='flex flex-row pl-2 mr-2'>
+                                {(frontmatter.authors || []).map((author: any, index: number) => {
+                                  return (
+                                    <div
+                                      key={index}
+                                      className='bg-slate-50 h-[44px] w-[44px] rounded-full -ml-3 bg-cover bg-center border-2 border-white'
+                                      style={{ backgroundImage: `url(${author.photo})`, zIndex: 10 - index }}
+                                    />
+                                  )
+                                })}
+                              </div>
+
+                              <div className='flex flex-col items-start'>
+                                <div className='text-sm font-semibold'>
+                                  {(frontmatter.authors.map((author: any) => author.name).join(' & '))}
+                                </div>
+
+                                <div className='text-slate-500 text-sm'>
+                                  {frontmatter.date && (
+                                    <span>
+                                      {date.toLocaleDateString('en-us', { year: 'numeric', month: 'long', day: 'numeric' })}
+                                    </span>
+                                  )} &middot; {timeToRead} min read
+                                </div>
+                              </div>
+
                             </div>
                           </div>
-                          <div className='flex flex-row items-center'>
-                            <div className='flex flex-row pl-2 mr-2'>
-                              {(frontmatter.authors || []).map((author: any, index: number) => {
-                                return (
-                                  <div
-                                    key={index}
-                                    className='bg-slate-50 h-[44px] w-[44px] rounded-full -ml-3 bg-cover bg-center border-2 border-white'
-                                    style={{ backgroundImage: `url(${author.photo})`, zIndex: 10 - index }}
-                                  />
-                                )
-                              })}
-                            </div>
-
-                            <div className='flex flex-col items-start'>
-                              <div className='text-sm font-semibold'>
-                                {(frontmatter.authors.map((author: any) => author.name).join(' & '))}
-                              </div>
-
-                              <div className='text-slate-500 text-sm'>
-                                {frontmatter.date && (
-                                  <span>
-                                    {date.toLocaleDateString('en-us', { year: 'numeric', month: 'long', day: 'numeric' })}
-                                  </span>
-                                )} &middot; {timeToRead} min read
-                              </div>
-                            </div>
-
-                          </div>
-                        </div>
-                      </a>
-                    </Link>
-                  </div>
+                        </a>
+                      </Link>
+                    </div>
+                  </>
                 )
               })
             }
