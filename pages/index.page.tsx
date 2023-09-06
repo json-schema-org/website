5c83adbab639071da9b4f4f86f637647053e22bf
Benjamin Granados <benjamin.granadosm@gmail.com>
2023-09-06 13:06:50 +0200

    Some minor corrections
    Some minor corrections

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -316,11 +316,11 @@ const Home = (props: any) => {
         <section className='my-20'>
           <div className='text-center mb-12'>
             <h2 className='text-h3mobile md:text-h3 font-semibold mb-4'>Supported by</h2>
-            <p className='px-4 md:w-1/2 mx-auto'>The following companies support us by letting us use their products.<br/><a href='mailto:ben@jsonschema.dev' className='border-b border-black'>Email us</a> for more info.</p>
+            <p className='px-4 md:w-1/2 mx-auto'>The following companies support us by letting us use their products.<br /><a href='mailto:ben@jsonschema.dev' className='border-b border-black'>Email us</a> for more info.</p>
           </div>
           <div className='flex justify-center items-center'>
-            <a href='https://orbit.love/'><img src='/img/logos/supported/new-orbit-logo-color.svg' className=' w-44'/></a>
-            <a href='https://json-schema.slack.com/join/shared_invite/zt-1ywpdj4yd-bXiBLjYEbKWUjzon0qiY9Q#/shared-invite/email'><img src='/img/logos/supported/slack-logo.svg' className=' w-44'/></a>
+            <a href='https://orbit.love/'><img src='/img/logos/supported/new-orbit-logo-color.svg' className=' w-44 h-auto' /></a>
+            <a href='https://json-schema.slack.com/join/shared_invite/zt-1ywpdj4yd-bXiBLjYEbKWUjzon0qiY9Q#/shared-invite/email'><img src='/img/logos/supported/slack-logo.svg' className=' w-44' /></a>
           </div>
         </section>
       </div>
