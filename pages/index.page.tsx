a55cb68217391703f6fcf384deb3717dc6db23ca
Benjamin Granados <benjamin.granadosm@gmail.com>
2023-10-13 11:10:57 +0200

    Contributing to JSON Schema

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -326,7 +326,7 @@ const Home = (props: any) => {
         {/* News & Blogs */}
         <section className='w-full h-[300px] lg:h-[367px] bg-gradient-to-r from-primary from-1.95% to-endBlue clip-both'>
           <div className='lg:w-full mx-auto text-center mt-28 '>
-            <h2 className='text-h3mobile lg:text-h3 text-white mb-6'>Contribute to the JSON Schema</h2>
+            <h2 className='text-h3mobile lg:text-h3 text-white mb-6'>Start contributing to JSON Schema</h2>
             <button className='w-[170px] h-[45px] mx-auto rounded border-2 bg-primary text-white font-semibold'><a href='https://github.com/json-schema-org#-contributing-to-json-schema
 '>Contribute</a></button>
           </div>
