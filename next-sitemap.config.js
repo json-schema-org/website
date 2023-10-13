75a13236e0aae702f7badb880e9a85653ce4c06a
Benjamin Granados <benjamin.granadosm@gmail.com>
2023-10-13 13:43:07 +0200

    Add sitemap using next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://json-schema.org/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
