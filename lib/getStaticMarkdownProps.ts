e07329ef8616ab869d77729c97ba735926519c10
Benjamin Granados <benjamin.granadosm@gmail.com>
2023-11-25 20:47:57 +0100

    Update getStaticMarkdownProps.ts

--- lib/getStaticMarkdownProps.ts
+++ lib/getStaticMarkdownProps.ts
@@ -3,10 +3,26 @@ import matter from 'gray-matter'
 
 type Props = { params?: { slug: string }}
 
+
+
 export default async function getStaticMarkdownProps(props: Props, path: string) {
   const slug = props.params?.slug || '_index'
-  const fileName = fs.readFileSync(`${path}/${slug}.md`, 'utf-8')
+  
+  const fileName2 = `${path}/${slug}.md`
+  const stats = fs.lstatSync(fileName2)
+  var fileName = null
+
+  if (stats.isSymbolicLink()){
+    fileName = fs.readFileSync(fileName2, 'utf-8')
+
+  } else {
+    fileName = fs.readFileSync(fileName2, 'utf-8')
+  }
+
   const { data: frontmatter, content } = matter(fileName)
+
+  console.log("Conten Data: " + content)
+
   return {
     props: {
       frontmatter,
