e67cbdc4b7f0e40a64d7ab8a687edf845e91814f
Benjamin Granados <benjamin.granadosm@gmail.com>
2023-11-26 11:04:49 +0100

    Update getStaticMarkdownProps.ts

--- lib/getStaticMarkdownProps.ts
+++ lib/getStaticMarkdownProps.ts
@@ -8,11 +8,8 @@ type Props = { params?: { slug: string }}
 export default async function getStaticMarkdownProps(props: Props, path: string) {
   const slug = props.params?.slug || '_index'
   
-  let fileName2 = `${path}/${slug}.md`
-  let fileName = null
-
-  fileName = fs.readFileSync(fileName2, 'utf-8')
-
+  const fileName2 = `${path}/${slug}.md`
+  const fileName = fs.readFileSync(fileName2, 'utf-8')
 
   const { data: frontmatter, content } = matter(fileName)
 
