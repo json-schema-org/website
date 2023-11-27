dc747a87f4ca1b8b43b07ace25f37fa4d69dfa9a
Benjamin Granados <benjamin.granadosm@gmail.com>
2023-11-27 10:31:57 +0100

    Improved call to actions for sponsorships

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -330,7 +330,8 @@ const Home = (props: any) => {
         <section className='my-16'>
           <div className='text-center mb-12'>
             <h2 className='text-h3mobile md:text-h3 font-semibold mb-2'>Sponsors</h2>
-            <p className='w-5/6 lg:w-3/5 mx-auto'>Want to become a sponsor? <a href='https://opencollective.com/json-schema' className='border-b border-black'>Support us!</a></p>
+            <p className='w-5/6 lg:w-3/5 mx-auto'>If you 🖤 JSON Schema consider becoming a <a href='https://opencollective.com/json-schema#category-CONTRIBUTE' className='border-b border-black'>sponsor</a> or a <a href='https://opencollective.com/json-schema#category-CONTRIBUTE' className='border-b border-black'>backer</a>.<br/><a href='https://opencollective.com/json-schema' className='border-b border-black'>Support us!</a>
+            </p>
           </div>
           <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-12 items-center mx-auto  md:mx-0 px-4'>
             <a href=' https://www.asyncapi.com/'><img src='/img/logos/sponsors/asyncapi-logo-dark.svg' className=' w-44' /></a>
