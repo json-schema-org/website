cb8e6067f778acfb80d6d41eec503daf00754d2a
AyushNautiyalDeveloper <ayush@betalectic.com>
2024-02-16 01:44:32 +0530

    removed the unwated code.

--- pages/newsletter/index.page.tsx
+++ pages/newsletter/index.page.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Navbar from '~/components/Molecules/navbarComponents/navbar'
 import NewsletterForm from '~/components/Newsletter'
-import Footer from '~/components/Molecules/footer'
+import Layout from '~/components/Layout'
 
 const index = () => {
   return (
     <>
-      <Navbar />
-      <NewsletterForm className='pt-[100px]' wrapperClassName='h-[calc(100vh-340px)]' />
-      <Footer />
+      <Layout>
+        <NewsletterForm className='pt-[100px]' wrapperClassName='h-[calc(100vh-340px)]' />
+      </Layout>
+    
     </>
   )
 }
