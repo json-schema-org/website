27ff58eeb48337f3e90bdfb5bb5572c9c39f22d1
AyushNautiyalDeveloper <ayush@betalectic.com>
2024-02-22 17:00:27 +0530

    Added the changes for the newsletter banner.

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -20,10 +20,10 @@ const NewsletterForm: React.FC<NewsletterFormProps> = ({ wrapperClassName = '', 
 
   return (
     <section
-      className={classnames('w-[100vw]  mx-auto flex items-center justify-center bg-[#090A11]', wrapperClassName)}>
-      <div className={classnames('w-full max-w-[900px] text-center px-5 py-9  bg-[#090A11] relative', className)}>
+      className={classnames('w-[100vw]  mx-auto flex items-center justify-center bg-white', wrapperClassName)}>
+      <div className={classnames('w-full max-w-[900px] text-center px-5 py-9  bg-white relative', className)}>
         
-        <h3 className=' font-bold tracking-heading mb-4 text-h3 px-5'>
+        <h3 className=' font-bold tracking-heading mb-4 text-h4 sm:text-h3  px-5'>
             Subscribe to our newsletter to receive news about Json Schema.
         </h3>
         <p
@@ -36,7 +36,7 @@ const NewsletterForm: React.FC<NewsletterFormProps> = ({ wrapperClassName = '', 
             type='text'
             name='username'
             placeholder='Your Name'
-            className='form-input block w-full py-3 text-lg sm:text-lg sm:leading-5  md:flex-1 rounded-md px-5 bg-gray-200'
+            className='form-input block w-full py-3 text-lg h-[40px] sm:h-[45px]  sm:text-lg sm:leading-5 border-2  md:flex-1 rounded px-5 bg-gray-200'
             value={username}
             onChange={(e: any) => setUsername(e.target.value)}
           />
@@ -44,13 +44,14 @@ const NewsletterForm: React.FC<NewsletterFormProps> = ({ wrapperClassName = '', 
             type='email'
             name='email'
             placeholder='Your Email'
-            className='form-input block w-full py-3 text-lg sm:text-lg sm:leading-5  md:flex-1 rounded-md px-5 bg-gray-200'
+            className='form-input block w-full py-3 text-lg sm:text-lg border-2 sm:leading-5  h-[40px] sm:h-[45px]  md:flex-1 rounded px-5 bg-gray-200'
             value={email}
             onChange={(e: any) => setEmail(e.target.value)}
           />
           <button
             type='submit'
-            className='bg-primary-500 hover:bg-primary-400 text-white bg-[#445cf4] transition-all duration-500 ease-in-out rounded-md px-4 py-3 text-md font-semibold tracking-heading w-full md:mt-0 md:flex-1'
+            className=' h-[40px] sm:h-[45px] mx-auto rounded border-2 bg-primary w-full text-white font-semibold md:mt-0 md:flex-1'
+            // className='bg-primary-500 hover:bg-primary-400 text-white bg-[#445cf4] transition-all duration-500 ease-in-out rounded-md px-4 py-3 text-md font-semibold tracking-heading w-full md:mt-0 md:flex-1'
           >
             <span className='inline-block'>Subscribe</span>
           </button>
--- pages/newsletter/index.page.tsx
+++ pages/newsletter/index.page.tsx
@@ -6,7 +6,7 @@ const index = () => {
   return (
     <>
       <Layout>
-        <NewsletterForm className='pt-[100px] bg-white text-black' wrapperClassName='h-[calc(100vh-340px)] bg-white' />
+        <NewsletterForm className='pt-[100px] bg-white text-black' wrapperClassName='h-full sm:h-[calc(100vh-340px)]  bg-white' />
       </Layout>
     
     </>
