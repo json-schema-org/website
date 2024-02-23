5a02af58dcc58b4face84978cf756bcabd470a87
aialok <alokgupta1560@gmail.com>
2024-02-23 19:32:34 +0530

    Formatted with prettier

--- lib/useSetUrlParam.ts
+++ lib/useSetUrlParam.ts
@@ -1,16 +1,16 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
-const HOST = 'https://json-schema.org'
+const HOST = 'https://json-schema.org';
 
-export default function useSetUrlParam () {
-  const router = useRouter()
-  const url = new URL(`${HOST}${router.asPath}`)
+export default function useSetUrlParam() {
+  const router = useRouter();
+  const url = new URL(`${HOST}${router.asPath}`);
   return (param: string, value: string | null) => {
     if (value === null) {
-      url.searchParams.delete(param)
+      url.searchParams.delete(param);
     } else {
-      url.searchParams.set(param, value)
+      url.searchParams.set(param, value);
     }
-    router.push(url.href.slice(HOST.length))
-  }
+    router.push(url.href.slice(HOST.length));
+  };
 }
--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -1,27 +1,43 @@
-import React from 'react'
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import React from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <Html lang='en' >
+      <Html lang='en'>
         <Head>
           <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
-          <link href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&family=JetBrains+Mono:wght@300;400&display=swap' rel='stylesheet' />
-          <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@docsearch/css@3' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin=''
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&family=JetBrains+Mono:wght@300;400&display=swap'
+            rel='stylesheet'
+          />
+          <link
+            rel='stylesheet'
+            href='https://cdn.jsdelivr.net/npm/@docsearch/css@3'
+          />
         </Head>
         <body className='relative'>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
-export default MyDocument
+export default MyDocument;
