7203987e81402016561faa7c3823fb79c3a1124b
officeneerajsaini <officeneerajsaini@gmail.com>
2024-02-29 17:24:17 +0530

    vertical alig icons

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -229,7 +229,7 @@ export const SegmentHeadline = ({ label }: { label: string }) => {
 
 const Footer = () => (
   <footer className={classnames('z-10 md:h-[300px]  bg-gradient-to-r from-startBlue from-1.95% to-endBlue clip-top grid items-center')}>
-    <div className='max-w-[1400px] mx-auto mt-4 grid grid-cols-1 md:grid-cols-2 md:w-1/2 lg:w-1/3 justify-center '>
+    <div className='max-w-[1400px] mx-auto mt-4 grid grid-cols-1 md:grid-cols-2 md:w-1/2 lg:w-1/3 justify-center'>
       <div className=' my-6 m-auto md:mt-16'>
         <img src='/img/logos/logo-white.svg' className='w-[150px] mb-6' />
         <div className='flex flex-col text-center sm:text-left'>
@@ -239,20 +239,20 @@ const Footer = () => (
           <a href='/overview/code-of-conduct' className='text-white mb-2'>Code of Conduct</a>
         </div>
       </div>
-      <div className='grid grid-cols-3 md:grid-cols-1 mx-auto md:mt-8 mb-4 md:mb-0 lg:ml-12'>
-        <div className='mr-4 mb-4'>
+      <div className='grid grid-cols-3 md:grid-cols-1 mx-auto md:mt-8 mb-4 md:mb-0 lg:ml-12 gap-x-2.5 gap-y-2 md:gap-x-0 md:gap-y-0'>
+        <div className=''>
           <a href='https://json-schema.org/slack' className='flex items-center text-white'><img src='/img/logos/slack_logo_small-white.svg' className='w-4 h-4 mr-2' />
             Slack</a>
         </div>
-        <div className='mb-4 mr-4'>
+        <div className=''>
           <a href='https://twitter.com/jsonschema' className='flex items-center text-white'><img src='/img/logos/x-twitter.svg' className='w-4 h-4 mr-2' />
             Twitter</a>
         </div>
-        <div className='mr-4 mb-4'>
+        <div className=''>
           <a href='https://linkedin.com/company/jsonschema/' className='flex items-center text-white'><img src='/img/logos/icons8-linkedin-2.svg' className='w-4 h-4 mr-2' />
             LinkedIn</a>
         </div>
-        <div className='mr-4 mb-4'>
+        <div className=''>
           <a href='https://www.youtube.com/@JSONSchemaOrgOfficial' className='flex items-center text-white'><img src='/img/logos/icons8-youtube.svg' className='w-4 h-4 mr-2' />
             Youtube</a>
         </div>
