10cc2c2309e44c7e76ea8633779c71008be1fb67
Aditya Sharma <sharma.aditya9164@gmail.com>
2024-03-03 11:35:17 +0530

    discard the changes in index.page.tsx file

--- pages/blog/index.page.tsx
+++ pages/blog/index.page.tsx
@@ -168,7 +168,6 @@ export default function StaticMarkdownPage({
             </div>
           </div>
         )}
-
         <div className='w-full mx-auto my-5'>
           <div className='flex h-full flex-col justify-center items-center mb-3 my-2'>
             <h2 className='text-h3mobile md:text-h3 font-bold px-4 items-center text-center'>
