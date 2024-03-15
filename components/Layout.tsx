9f92fe001a504e30516f0aedc7320fe735f2d69d
Aditya Sharma <sharma.aditya9164@gmail.com>
2024-03-15 11:10:41 +0530

    handled the overflow issue while opening search bar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -48,7 +48,7 @@ export default function Layout({
 
   const newTitle = `JSON Schema${metaTitle ? ` - ${metaTitle}` : ''}`;
   return (
-    <div className='min-h-screen relative flex flex-col justify-between'>
+    <div className='min-h-screen fixed inset-0 w-full overflow-y-scroll flex flex-col justify-betweenn'>
       <FaviconHead />
       <Head>
         <title>{newTitle}</title>
@@ -67,7 +67,7 @@ export default function Layout({
         >
           <header
             className={classnames(
-              'w-full bg-white fixed top-0 z-[170] shadow-xl drop-shadow-lg',
+              'w-screen fixed bg-white fixed top-0 z-[170] shadow-xl drop-shadow-lg',
             )}
           >
             <div className='w-full flex md:justify-between items-center ml-8 2xl:px-12 py-4'>
