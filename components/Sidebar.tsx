023a3c066464a2c277b0be0e28e918f20dff40cd
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2024-03-20 16:32:56 +0530

    feat: added resource section (#509)
    * fixed
    
    * feat: added resource section
    
    * feat: added resource section
    
    * revert changes cd6d848
    
    * revert changes 65e9f3e
    
    * revert changes 877b0f0
    
    * revert changes 74711cd
    
    * fix: fixed the width issue
    
    * revert change
    
    * fix: added resource icon
    
    * Tiny changes to adjust look and feel.
    
    * Last changes to fix dark theme
    
    ---------
    
    Co-authored-by: Benjamin Granados <benjamin.granadosm@gmail.com>

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -125,6 +125,22 @@ const getSpecificationPath = [
   '/specification-links',
   '/specification',
 ];
+const getResourcePath = [
+  '/resources/articles',
+  '/resources/books',
+  '/resources/articles',
+  '/resources/podcasts',
+  '/resources/papers',
+  '/resources/audios',
+];
+const resourceRoutes = [
+  { uri: '/resources/books', label: 'Books' },
+  { uri: '/resources/articles', label: 'Articles' },
+  { uri: '/resources/courses', label: 'Courses' },
+  { uri: '/resources/videos', label: 'Videos' },
+  { uri: '/resources/podcasts', label: 'Podcasts' },
+  { uri: '/resources/papers', label: 'Papers' },
+];
 export const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const [open, setOpen] = useState(false);
@@ -184,13 +200,14 @@ export const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
         </div>
 
         <div
-          className={`z-[150] absolute top-10 mt-24 left-0 h-full w-screen bg-white transform ${open ? '-translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out filter drop-shadow-md `}
+          className={`z-[150] absolute top-10 mt-24 left-0 h-full w-screen bg-white transform ${
+            open ? '-translate-x-0' : '-translate-x-full'
+          } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
         >
           <div className='flex flex-col mt-4'>
             <DocsNav />
           </div>
         </div>
-
         <div className='dark:bg-slate-800 max-w-[1400px] grid grid-cols-1 lg:grid-cols-4 mx-4 md:mx-12'>
           <div className='hidden lg:block mt-24'>
             <DocsNav />
@@ -217,6 +234,7 @@ export const DocsNav = () => {
     getStarted: false,
     getReference: false,
     getSpecification: false,
+    getResources: false,
   });
   useEffect(() => {
     const pathWtihoutFragment = extractPathWithoutFragment(router.asPath);
@@ -228,6 +246,8 @@ export const DocsNav = () => {
       setActive({ ...active, getReference: true });
     } else if (getSpecificationPath.includes(pathWtihoutFragment)) {
       setActive({ ...active, getSpecification: true });
+    } else if (getResourcePath.includes(router.asPath)) {
+      setActive({ ...active, getResources: true });
     }
   }, [router.asPath]);
 
@@ -247,10 +267,15 @@ export const DocsNav = () => {
     setActive({ ...active, getSpecification: !active.getSpecification });
   };
 
+  const handleClickResources = () => {
+    setActive({ ...active, getResources: !active.getResources });
+  };
+
   const rotate = active.getDocs ? 'rotate(180deg)' : 'rotate(0)';
   const rotateG = active.getStarted ? 'rotate(180deg)' : 'rotate(0)';
   const rotateR = active.getReference ? 'rotate(180deg)' : 'rotate(0)';
   const rotateSpec = active.getSpecification ? 'rotate(180deg)' : 'rotate(0)';
+  const rotateResources = active.getResources ? 'rotate(180deg)' : 'rotate(0)';
 
   const { theme } = useTheme();
 
@@ -258,6 +283,7 @@ export const DocsNav = () => {
   const [reference_icon, setReference_icon] = useState('');
   const [spec_icon, setSpec_icon] = useState('');
   const [overview_icon, setOverview_icon] = useState('');
+  const [resources_icon, setResources_icon] = useState('');
 
   useEffect(() => {
     if (theme === 'dark') {
@@ -265,11 +291,13 @@ export const DocsNav = () => {
       setLearn_icon('/icons/compass-dark.svg');
       setReference_icon('/icons/book-dark.svg');
       setSpec_icon('/icons/clipboard-dark.svg');
+      setResources_icon('/icons/bookshelf-dark.svg');
     } else {
       setOverview_icon('/icons/eye.svg');
       setLearn_icon('/icons/compass.svg');
       setReference_icon('/icons/book.svg');
       setSpec_icon('/icons/clipboard.svg');
+      setResources_icon('/icons/bookshelf.svg');
     }
   }, [theme]);
 
@@ -573,6 +601,50 @@ export const DocsNav = () => {
               label='draft-06 notes'
             />
           </div>
+        </div>
+      </div>
+      {/* Resources */}
+      <div className='mb-2 bg-slate-200 dark:bg-slate-900 p-2 rounded'>
+        <div
+          className='flex justify-between w-full items-center'
+          onClick={handleClickResources}
+        >
+          <div className='flex  items-center align-middle'>
+            <img
+              src={`${resources_icon}`}
+              alt='eye icon'
+              className='mr-2 w-6'
+            />
+            <SegmentHeadline label='Resources' />
+          </div>
+          <svg
+            id='arrow'
+            className='arrow'
+            style={{
+              transform: rotateResources,
+              transition: 'all 0.2s linear',
+            }}
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            height='32'
+            viewBox='0 0 24 24'
+            width='24'
+          >
+            <path
+              clipRule='evenodd'
+              d='m16.5303 8.96967c.2929.29289.2929.76777 0 1.06063l-4 4c-.2929.2929-.7677.2929-1.0606 0l-4.00003-4c-.29289-.29286-.29289-.76774 0-1.06063s.76777-.29289 1.06066 0l3.46967 3.46963 3.4697-3.46963c.2929-.29289.7677-.29289 1.0606 0z'
+              fill='#707070'
+              fillRule='evenodd'
+            />
+          </svg>
+        </div>
+        <div
+          className={classnames('ml-6', { hidden: !active.getResources })}
+          id='resources'
+        >
+          {resourceRoutes.map(({ uri, label }) => (
+            <DocLink key={uri} uri={uri} label={label} />
+          ))}
         </div>
       </div>
     </div>
