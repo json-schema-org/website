532d51f6b55d9f28285ee16c6dc87052814d8dd9
Sahil Shadwal <sahilshadwal@gmail.com>
2024-03-22 11:49:29 +0530

    Added #website and slack link to feedback

--- components/DocsHelp.tsx
+++ components/DocsHelp.tsx
@@ -214,7 +214,38 @@ export function DocsHelp() {
 
           {feedbackStatus === 'feedback' && (
             <div className='my-6 text-[14px]'>
-              <p>Thank you! We received your feedback.</p>
+              <p>
+                Thanks for the feedback! See it on&nbsp;
+                <a
+                  target='_blank'
+                  rel='noreferrer'
+                  className='underline'
+                  href='https://json-schema.slack.com/archives/C8C4UBXDF'
+                >
+                  #website
+                </a>
+                &nbsp;in&nbsp;
+                <a
+                  target='_blank'
+                  rel='noreferrer'
+                  className='underline'
+                  href='https://json-schema.org/slack'
+                >
+                  <svg
+                    className='inline-block select-none align-text-bottom mr-1'
+                    aria-hidden='true'
+                    role='img'
+                    viewBox='0 0 16 16'
+                    width='16'
+                    height='16'
+                    fill='currentColor'
+                  >
+                    <path d='M1.75 1h8.5c.966 0 1.75.784 1.75 1.75v5.5A1.75 1.75 0 0 1 10.25 10H7.061l-2.574 2.573A1.458 1.458 0 0 1 2 11.543V10h-.25A1.75 1.75 0 0 1 0 8.25v-5.5C0 1.784.784 1 1.75 1ZM1.5 2.75v5.5c0 .138.112.25.25.25h1a.75.75 0 0 1 .75.75v2.19l2.72-2.72a.749.749 0 0 1 .53-.22h3.5a.25.25 0 0 0 .25-.25v-5.5a.25.25 0 0 0-.25-.25h-8.5a.25.25 0 0 0-.25.25Zm13 2a.25.25 0 0 0-.25-.25h-.5a.75.75 0 0 1 0-1.5h.5c.966 0 1.75.784 1.75 1.75v5.5A1.75 1.75 0 0 1 14.25 12H14v1.543a1.458 1.458 0 0 1-2.487 1.03L9.22 12.28a.749.749 0 0 1 .326-1.275.749.749 0 0 1 .734.215l2.22 2.22v-2.19a.75.75 0 0 1 .75-.75h1a.25.25 0 0 0 .25-.25Z' />
+                  </svg>
+                  Slack&nbsp;
+                </a>
+                for further discussion.
+              </p>
             </div>
           )}
 
