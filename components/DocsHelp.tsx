16c266768d91b4054803e06a7f3b182909e671d7
Sahil Shadwal <sahilshadwal@gmail.com>
2024-03-22 13:00:25 +0530

    changed the message to be more humble

--- components/DocsHelp.tsx
+++ components/DocsHelp.tsx
@@ -215,7 +215,7 @@ export function DocsHelp() {
           {feedbackStatus === 'feedback' && (
             <div className='my-6 text-[14px]'>
               <p>
-                Thanks for the feedback! See it on&nbsp;
+                Thanks for the feedback! Feel free to join on&nbsp;
                 <a
                   target='_blank'
                   rel='noreferrer'
