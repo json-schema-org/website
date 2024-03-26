0337e7cf2b9a4e4c0e13bdd69ca8ad7e02754230
its-VinayKumar <129538020+its-VinayKumar@users.noreply.github.com>
2024-03-26 06:31:59 +0000

    Copyright Added in the Footer Section.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -367,7 +367,10 @@ const Footer = () => (
             GitHub
           </a>
         </div>
-      </div>
+       </div>
+       </div>
+      <div className='copyright-section'>
+        <p>Copyright © 2024 JSON Schema. All rights reserved</p>
     </div>
   </footer>
 );
