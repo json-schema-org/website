8088056693ad02f6e5da776d0635eb552c83ded1
Sahil Shadwal <sahilshadwal@gmail.com>
2024-04-11 09:37:50 +0530

    fix  the href for index.page.tsx

--- pages/understanding-json-schema/index.page.tsx
+++ pages/understanding-json-schema/index.page.tsx
@@ -20,7 +20,7 @@ export async function getStaticProps() {
 }
 
 export default function ContentExample({ blocks }: { blocks: any[] }) {
-  const markdownFile = '_index';
+  const markdownFile = '_indexPage';
 
   return (
     <SectionContext.Provider value='docs'>
