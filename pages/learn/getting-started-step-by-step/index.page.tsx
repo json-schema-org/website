4d79def9cdd1f48edd62ac85901efb08c5d22a1d
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-05-30 23:40:35 +0530

    adjusting layout

--- pages/learn/getting-started-step-by-step/index.page.tsx
+++ pages/learn/getting-started-step-by-step/index.page.tsx
@@ -52,13 +52,11 @@ export default function StyledValidator({
       <StyledMarkdown markdown={blocks[0]} />
 
       <div className='flex flex-col'>
-        <div className='flex flex-row justify-between mt-5'>
-          <h1 className='text-h4 font-medium max-sm:text-[12px]'>
-            Select a JSON Schema Validator
-          </h1>
+        <div className='flex items-center flex-row justify-between mt-5 mb-3 '>
+          <h2 className='text-h5 font-semibold'>JSON Schema</h2>
           <select
-            name='Getting Started Examples'
-            className='p-2 dark:bg-slate-900 rounded-md max-sm:text-[12px]'
+            name='Select a JSON Schema Validator'
+            className='p-2 border dark:border-slate-300 border-slate-800 dark:bg-slate-900 rounded-md max-sm:text-[12px]'
             id='Examples'
           >
             {data.map((data, id) => {
@@ -75,24 +73,15 @@ export default function StyledValidator({
           </select>
         </div>
 
-        <p className='my-5 leading-7'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-          tinctio sint voluptate.
-        </p>
-
-        <h2 className='text-h5'>JSON Schema</h2>
-
         <JsonEditor initialCode={schemaData} />
       </div>
 
       <div className='flex flex-col'>
-        <div className='flex flex-row justify-between my-5'>
-          <h1 className='text-h4 max-sm:text-[12px] font-medium'>
-            Select a JSON Schema Instance
-          </h1>
+        <div className='flex items-center flex-row justify-between mb-3 '>
+          <h2 className='text-h5 font-semibold'>Instance</h2>
           <select
-            name='Getting Started Examples'
-            className='p-2  dark:bg-slate-900 rounded-md max-sm:text-[12px] '
+            name='Select a JSON Schema Instance'
+            className='p-2 border dark:border-slate-300 border-slate-800 dark:bg-slate-900 rounded-md max-sm:text-[12px] '
             id='Examples'
           >
             {data.map((data, id) => {
@@ -109,15 +98,24 @@ export default function StyledValidator({
           </select>
         </div>
 
-        <h2 className='text-h5'>Instance</h2>
         <JsonEditor initialCode={instanceData} />
       </div>
 
-      <h2 className='text-h5'>Result</h2>
+      <h2 className='text-h5 font-semibold'>Result</h2>
       <div className='flex bg-slate-800 justify-between items-center text-white font-medium flex-row border p-5 rounded-xl'>
-        <p>This is a valid JSON instance for the provided JSON Schema</p>
+        {data.map((data, id) => {
+          return (
+            <>
+              <p key={id}>{data.instances[0].details}</p>
 
-        <img src='/icons/green-tick.svg' alt='' />
+              {data.instances[0].valid ? (
+                <img src='/icons/green-tick.svg' alt='green tick' />
+              ) : (
+                <img src='/icons/red-cross.svg' alt='red cross' />
+              )}
+            </>
+          );
+        })}
       </div>
       <div className='flex justify-end'>
         <button className='px-3 py-2 text-white rounded-md mt-5 bg-startBlue hover:bg-startBlue/90'>
