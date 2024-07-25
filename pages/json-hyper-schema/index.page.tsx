1465e80f152f7bf74df075f0cbe9c19472165a01
Blessing Ene Anyebe <anyebeblessing737@yahoo.com>
2024-07-25 14:45:34 +0100

    Modified json hyper-schema page

--- pages/json-hyper-schema/index.page.tsx
+++ pages/json-hyper-schema/index.page.tsx
@@ -4,6 +4,7 @@ import fs from 'fs';
 import matter from 'gray-matter';
 import StyledMarkdown from '~/components/StyledMarkdown';
 import { SectionContext } from '~/context';
+import { Headline1 } from '~/components/Headlines';
 
 export async function getStaticProps() {
   const index = fs.readFileSync('pages/json-hyper-schema/index.md', 'utf-8');
@@ -33,7 +34,7 @@ export default function ImplementationsPages({
   return (
     <SectionContext.Provider value={null}>
       <div className='w-5/6 mx-auto mt-40 dark:text-slate-200'>
-        <h1>{frontmatter.title}</h1>
+      <Headline1>{frontmatter.title}</Headline1>
         <h1>{frontmatter.type}</h1>
         <h2>{frontmatter.Specification}</h2>
 
