b69f07a8cda67521563453d8125f89e42892f3d1
29deepanshutyagi <29deepanshutyagi@gmail.com>
2024-10-04 17:19:13 +0530

    fix: remove redundant class from JSON Schema Validator select elements

--- components/GettingStarted.tsx
+++ components/GettingStarted.tsx
@@ -135,7 +135,7 @@ const GettingStarted = () => {
               </label>
               <select
                 name='Select a JSON Schema Validator'
-                className='p-2 border dark:border-slate-300 border-slate-800 dark:bg-slate-900 rounded-md max-sm:text-[12px] plausible-event-name==activation-explore-tools'
+                className='p-2 border dark:border-slate-300 border-slate-800 dark:bg-slate-900 rounded-md max-sm:text-[12px]'
                 id='Examples'
                 onChange={handleSchemaChange}
               >
@@ -190,7 +190,7 @@ const GettingStarted = () => {
               </label>
               <select
                 name='Select a JSON Schema Validator'
-                className='p-2 border dark:border-slate-300 border-slate-800 dark:bg-slate-900 rounded-md max-sm:text-[12px] plausible-event-name==activation-explore-tools'
+                className='p-2 border dark:border-slate-300 border-slate-800 dark:bg-slate-900 rounded-md max-sm:text-[12px]'
                 id='Examples'
                 onChange={handleInstanceChange}
               >
