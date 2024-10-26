28412f05a7eb2ffda4871a27f8b7c29974c0fcc8
Ritesh Hiremath <ritesh@Riteshs-MacBook-Air.local>
2024-10-26 22:43:09 +0530

    Removing the safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,6 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,md}',
   ],
   darkMode: 'class',
-  safelist: [
-    'w-8',
-    'h-11',
-    'w-11',
-    'bg-slate-50',
-    'rounded-full',
-    '-ml-3',
-    'bg-cover',
-    'bg-center',
-    'border-2',
-    'border-white',
-    'h-8',
-  ],
   theme: {
     screens: {
       sm: '640px',
