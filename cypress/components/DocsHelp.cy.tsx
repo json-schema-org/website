6f7403dc0467051ef46ddf15942b5aa85c60cea9
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2024-11-04 13:54:37 +0530

    test-command-add-check

--- cypress/components/DocsHelp.cy.tsx
+++ cypress/components/DocsHelp.cy.tsx
@@ -10,17 +10,13 @@ const FEEDBACK_FORM_NO_BUTTON = '[data-test="feedback-survey-no-button"]';
 const FEEDBACK_FORM_INPUT = '[data-test="feedback-form-input"]';
 const FEEDBACK_FORM_SUBMIT_BUTTON = '[data-test="feedback-submit-button"]';
 const CREATE_GITHUB_ISSUE_BUTTON = '[data-test="create-github-issue-button"]';
-const FEEDBACK_FORM_SUCCESS_MESSAGE =
-  '[data-test="feedback-form-success-message"]';
+const FEEDBACK_FORM_SUCCESS_MESSAGE = '[data-test="feedback-form-success-message"]';
 const FEEDBACK_ERROR_MESSAGE = '[data-test="feedback-form-error-message"]';
-const FEEDBACK_FORM_GITHUB_SUCCESS_MESSAGE =
-  '[data-test="feedback-form-github-success-message"]';
+const FEEDBACK_FORM_GITHUB_SUCCESS_MESSAGE = '[data-test="feedback-form-github-success-message"]';
 
 // DocsHelp Component
 describe('DocsHelp Component', () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let mockRouter: MockRouter;
-  // Note: we are not using the mockRouter in this test file, but it is required to mock the router in the component file
 
   beforeEach(() => {
     const markdownFile = '_md';
@@ -29,223 +25,144 @@ describe('DocsHelp Component', () => {
     cy.mount(<DocsHelp fileRenderType={markdownFile} />);
   });
 
-  // should render the component correctly
   it('should render the component correctly', () => {
-    // Check if the main component wrapper is present
     cy.get(DOCS_HELP).should('exist');
 
-    // "Need Help?" header
     cy.get('[data-test="need-help-heading"]')
       .should('have.prop', 'tagName', 'H2')
-      .and('contains', /Need Help?/i);
+      .and('contain.text', 'Need Help?');
 
-    // Main feedback question
     cy.get('[data-test="feedback-main-heading"]')
       .should('have.prop', 'tagName', 'H3')
-      .and('contains', /Did you find these docs helpful?/i);
+      .and('contain.text', 'Did you find these docs helpful?');
 
-    // Feedback form element
     cy.get(FEEDBACK_FORM).should('have.prop', 'tagName', 'FORM');
 
-    // "Help us improve" section header
     cy.get('[data-test="contribute-docs-heading"]')
       .should('have.prop', 'tagName', 'H3')
-      .and('contains', /Help us make our docs great!/i);
+      .and('contain.text', 'Help us make our docs great!');
 
-    // Contribution encouragement text
     cy.get('[data-test="contribute-docs-description"]')
       .should('have.prop', 'tagName', 'P')
-      .and(
-        'contains',
-        /At JSON Schema, we value docs contributions as much as every other type of contribution!/i,
-      );
+      .and('contain.text', 'At JSON Schema, we value docs contributions');
 
-    // "Edit on GitHub" link
     cy.get('[data-test="edit-on-github-link"]')
       .should('have.prop', 'tagName', 'A')
-      .and('contains', /Edit this page on Github/i);
+      .and('contain.text', 'Edit this page on Github');
 
-    // "Learn to contribute" link
     cy.get('[data-test="learn-to-contribute-link"]')
       .should('have.prop', 'tagName', 'A')
-      .and(
-        'have.attr',
-        'href',
-        'https://github.com/json-schema-org/website/blob/main/CONTRIBUTING.md',
-      )
-      .and('contains', /Learn how to contribute/i);
+      .and('have.attr', 'href', 'https://github.com/json-schema-org/website/blob/main/CONTRIBUTING.md')
+      .and('contain.text', 'Learn how to contribute');
 
-    // "Still Need Help?" section header
     cy.get('[data-test="additional-help-heading"]')
       .should('have.prop', 'tagName', 'H3')
-      .and('contains', /Still Need Help?/i);
+      .and('contain.text', 'Still Need Help?');
 
-    // Additional help description
     cy.get('[data-test="additional-help-description"]')
       .should('have.prop', 'tagName', 'P')
-      .should(
-        'contains',
-        /Learning JSON Schema is often confusing, but don't worry, we are here to help!./i,
-      );
+      .and('contain.text', "Learning JSON Schema is often confusing");
 
-    // GitHub community link
-    cy.get('[ data-test="ask-on-github-link"]')
+    cy.get('[data-test="ask-on-github-link"]')
       .should('have.prop', 'tagName', 'A')
-      .and(
-        'have.attr',
-        'href',
-        'https://github.com/orgs/json-schema-org/discussions/new?category=q-a',
-      )
-      .and('contains', /Ask the community on GitHub/i);
+      .and('have.attr', 'href', 'https://github.com/orgs/json-schema-org/discussions/new?category=q-a')
+      .and('contain.text', 'Ask the community on GitHub');
 
-    // Slack community link
     cy.get('[data-test="ask-on-slack-link"]')
       .should('have.prop', 'tagName', 'A')
       .and('have.attr', 'href', 'https://json-schema.org/slack')
-      .and('contains', /Ask the community on Slack/i);
+      .and('contain.text', 'Ask the community on Slack');
   });
 
-  // test feedback form funtionality works correctly
   it('should handle successful feedback submission', () => {
-    // mocking the feedback api call
     cy.intercept(
       'POST',
       'https://script.google.com/macros/s/AKfycbx9KA_BwTdsYgOfTLrHAxuhHs_wgYibB5_Msj9XP1rL5Ip4A20g1O609xAuTZmnbhRv/exec',
-      {
-        statusCode: 200,
-        body: { success: true },
-      },
+      { statusCode: 200, body: { success: true } }
     ).as('feedback');
 
-    /* click on yes button and check if feedback form is visible
-       Note: checking both yes and no button to cover both scenarios */
     cy.get(FEEDBACK_FORM_YES_BUTTON).click();
     cy.get(FEEDBACK_FORM_NO_BUTTON).click();
     cy.get(FEEDBACK_FORM).should('be.visible');
 
-    // now type in feedback form and submit
     cy.get(FEEDBACK_FORM_INPUT).type('JSON Schema is awesome');
     cy.get(FEEDBACK_FORM_SUBMIT_BUTTON).click();
 
-    // check if response status code is 200
     cy.wait('@feedback').its('response.statusCode').should('eq', 200);
-
-    // check if clicking on submit button should show success message
     cy.get(FEEDBACK_FORM_SUCCESS_MESSAGE)
       .should('have.prop', 'tagName', 'P')
-      .and('contains', /Thank you for your feedback!/i);
+      .and('contain.text', 'Thank you for your feedback!');
   });
 
-  /* test feedback form functionality when status code is 500
-    Note: This is case when server returns an error response | eg: INTERNAL SERVER ERROR */
   it('should handle API error response', () => {
-    // check if clicking on yes button should show feedback form
     cy.intercept(
       'POST',
       'https://script.google.com/macros/s/AKfycbx9KA_BwTdsYgOfTLrHAxuhHs_wgYibB5_Msj9XP1rL5Ip4A20g1O609xAuTZmnbhRv/exec',
-      {
-        statusCode: 500,
-        body: { error: 'Internal Server Error' },
-      },
+      { statusCode: 500, body: { error: 'Internal Server Error' } }
     ).as('feedback');
 
-    // click on yes button and check if feedback form is visible
     cy.get(FEEDBACK_FORM_YES_BUTTON).click();
     cy.get(FEEDBACK_FORM).should('be.visible');
-
-    // now type in feedback form and submit
     cy.get(FEEDBACK_FORM_INPUT).type('JSON Schema is awesome');
     cy.get(FEEDBACK_FORM_SUBMIT_BUTTON).click();
 
-    // check if response status code is 500
     cy.wait('@feedback').its('response.statusCode').should('eq', 500);
-
-    // check if clicking on submit button should show error message
     cy.get(FEEDBACK_ERROR_MESSAGE)
       .should('have.prop', 'tagName', 'P')
-      .and('contains', /An error occurred. Please try again later./i);
+      .and('contain.text', 'An error occurred. Please try again later.');
   });
 
-  /* test feedback form functionality when network error occurs
-    Note: This is case when network error occurs while sending request to server | eg: NO INTERNET CONNECTION */
   it('should handle network error', () => {
-    // check if clicking on yes button should show feedback form
     cy.intercept(
       'POST',
       'https://script.google.com/macros/s/AKfycbx9KA_BwTdsYgOfTLrHAxuhHs_wgYibB5_Msj9XP1rL5Ip4A20g1O609xAuTZmnbhRv/exec',
-      {
-        forceNetworkError: true,
-      },
+      { forceNetworkError: true }
     ).as('feedback');
 
-    // click on yes button and check if feedback form is visible
     cy.get(FEEDBACK_FORM_YES_BUTTON).click();
     cy.get(FEEDBACK_FORM).should('be.visible');
-
-    // now type in feedback form and submit
     cy.get(FEEDBACK_FORM_INPUT).type('JSON Schema is awesome');
     cy.get(FEEDBACK_FORM_SUBMIT_BUTTON).click();
 
-    // check if clicking on submit button should show error message
     cy.get(FEEDBACK_ERROR_MESSAGE)
       .should('have.prop', 'tagName', 'P')
-      .and('contains', /An error occurred. Please try again later./i);
+      .and('contain.text', 'An error occurred. Please try again later.');
   });
 
-  // test create github issue functionality when submitting feedback
-  it('should open github issue page', () => {
-    // mock window.open function
+  it('should open GitHub issue page', () => {
     cy.window().then((win) => {
       cy.stub(win, 'open').as('windowOpen');
     });
-    // check if clicking on yes button should show feedback form
     cy.get(FEEDBACK_FORM_YES_BUTTON).click();
     cy.get(FEEDBACK_FORM).should('be.visible');
-
-    // now type in feedback form and submit
     cy.get(FEEDBACK_FORM_INPUT).type('JSON Schema is awesome');
     cy.get(CREATE_GITHUB_ISSUE_BUTTON).click();
 
-    // check if clicking on submit button should show success message
     cy.get(FEEDBACK_FORM_GITHUB_SUCCESS_MESSAGE)
       .should('have.prop', 'tagName', 'P')
-      .and(
-        'contains',
-        /Thanks for creating an issue! Let's continue the discussion there!/i,
-      );
+      .and('contain.text', "Thanks for creating an issue!");
   });
 
-  // test show error message when github issue page fails to open
-  it('should handle error while opening github issue page', () => {
-    // mock window.open function with error
+  it('should handle error while opening GitHub issue page', () => {
     cy.window().then((win) => {
       cy.stub(win, 'open').throws(new Error('Test error'));
     });
 
-    // check if clicking on yes button should show feedback form
     cy.get(FEEDBACK_FORM_YES_BUTTON).click();
     cy.get(FEEDBACK_FORM).should('be.visible');
-
-    // now type in feedback form and submit
     cy.get(FEEDBACK_FORM_INPUT).type('JSON Schema is awesome');
     cy.get(CREATE_GITHUB_ISSUE_BUTTON).click();
 
-    // check if clicking on submit button should show error message
     cy.get(FEEDBACK_ERROR_MESSAGE)
       .should('have.prop', 'tagName', 'P')
-      .and('contains', /An error occurred. Please try again later./i);
+      .and('contain.text', 'An error occurred. Please try again later.');
   });
 
-  // This test is to check component render correctly with different markdown files
   it('should render component with different markdown files', () => {
-    /* Note: Already checking with _index markdown file in the first test case */
-
-    // render with _indexPage markdown file
     const markdownFile = 'tsx';
     cy.mount(<DocsHelp fileRenderType={markdownFile} />);
     cy.get(DOCS_HELP).should('exist');
 
-    // render without any markdown file
     cy.mount(<DocsHelp />);
   });
 });
