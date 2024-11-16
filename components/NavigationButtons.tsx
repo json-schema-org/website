575365a72cc909f785be99f973b0a27769123c44
Ronak Surana <100184606+RonakSurana-2001@users.noreply.github.com>
2024-11-16 09:27:18 +0000

    Correct NavButtons Build Error

--- components/NavigationButtons.tsx
+++ components/NavigationButtons.tsx
@@ -63,25 +63,31 @@ export default function NextPrevButton({
         <div className='h-auto w-1/2'></div>
       )}
 
-      <div className='h-auto w-1/2'>
-        <div className='h-full cursor-pointer rounded border border-gray-200 p-4 text-center shadow-md transition-all duration-300 ease-in-out hover:border-gray-300 hover:shadow-lg dark:shadow-xl dark:drop-shadow-lg dark:hover:shadow-2xl lg:text-right'>
-          <Link href={nextURL}>
-            <div className='text-primary  dark:text-slate-300 flex flex-row-reverse gap-5 text-[18px]'>
-              <Image
-                src={'/icons/arrow.svg'}
-                height={10}
-                width={10}
-                alt='next icon '
-                className='w-5'
-              />
-              <div className='my-auto inline font-bold uppercase '>Up Next</div>
-            </div>
-            <div className='my-2 text-base font-medium text-slate-600 dark:text-slate-300'>
-              {nextLabel}
-            </div>
-          </Link>
+      {nextURL && nextLabel ? (
+        <div className='h-auto w-1/2'>
+          <div className='h-full cursor-pointer rounded border border-gray-200 p-4 text-center shadow-md transition-all duration-300 ease-in-out hover:border-gray-300 hover:shadow-lg dark:shadow-xl dark:drop-shadow-lg dark:hover:shadow-2xl lg:text-right'>
+            <Link href={nextURL}>
+              <div className='text-primary  dark:text-slate-300 flex flex-row-reverse gap-5 text-[18px]'>
+                <Image
+                  src={'/icons/arrow.svg'}
+                  height={10}
+                  width={10}
+                  alt='next icon '
+                  className='w-5'
+                />
+                <div className='my-auto inline font-bold uppercase '>
+                  Up Next
+                </div>
+              </div>
+              <div className='my-2 text-base font-medium text-slate-600 dark:text-slate-300'>
+                {nextLabel}
+              </div>
+            </Link>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className='h-auto w-1/2'></div>
+      )}
     </div>
   );
 }
