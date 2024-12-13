893f6c73ca950aacbf8dc806ede596cf31313c0e
Benjamin Granados <40007659+benjagm@users.noreply.github.com>
2024-12-13 23:12:08 +0100

    more tests

--- pages/ambassadors/components/AmbassadorBanner.tsx
+++ pages/ambassadors/components/AmbassadorBanner.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AmbassadorBanner: React.FC = () => {
   return (
     <div className='flex justify-center mx-4 md:mx-10 my-8 w-full'>
-      <div className='group relative h-auto w-full md:w-5/6 lg:w-2/3 xl:w-1/2 rounded-lg border border-gray-200 bg-white p-6 px-12 shadow-3xl dark:shadow-2xl dark:shadow-slate-900 transition-colors ease-in-out hover:bg-slate-100 dark:bg-slate-800 hover:dark:bg-slate-900/30'>
+      <div className='group relative h-auto md:w-5/6 lg:w-2/3 xl:w-1/2 rounded-lg border border-gray-200 bg-white p-6 px-12 shadow-3xl dark:shadow-2xl dark:shadow-slate-900 transition-colors ease-in-out hover:bg-slate-100 dark:bg-slate-800 hover:dark:bg-slate-900/30'>
         <h3 className='text-2xl font-bold mb-2 text-gray-800 dark:text-slate-100'>
           Become a JSON Schema Ambassador
         </h3>
@@ -13,7 +13,7 @@ const AmbassadorBanner: React.FC = () => {
           you're selected, you'll join JSON Schema's mission of helping
           community members all over the world build the future of JSON Schema.
         </p>
-        <div className='w-full lg:w-full grid grid-cols-2 sm:grid-cols-2 gap-6 my-[10px] mx-auto mt-8'>
+        <div className='w-full lg:w-full grid grid-cols-2 sm:grid-cols-2 gap-6 my-[10px] mx-auto mt-8 '>
           <Link
             href='https://github.com/json-schema-org/community/tree/main/programs/ambassadors#become-an-json-schema-ambassador'
             className='inline-block px-6 py-3 bg-blue-600 text-white font-semibold text-center rounded hover:bg-blue-700 transition duration-300'
