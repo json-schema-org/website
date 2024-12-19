61ef65347ec1193cd84812b26af1156b0dcbd2ea
Utkarsh Tiwari <utk0703@gmail.com>
2024-12-20 00:24:50 +0530

    fixed some other broken links such as faq, case studies, roadmap and use cases page

--- pages/overview/case-studies/index.page.tsx
+++ pages/overview/case-studies/index.page.tsx
@@ -10,7 +10,7 @@ import { useTheme } from 'next-themes';
 
 export default function ContentExample() {
   const newTitle = 'Case Studies';
-  const markdownFile = '_indexPage';
+  // const markdownFile = '_indexPage';
   const { resolvedTheme } = useTheme();
 
   const imgUrl = (src: string): string => {
@@ -47,7 +47,7 @@ export default function ContentExample() {
           />
         ))}
       </div>
-      <DocsHelp markdownFile={markdownFile} />
+      <DocsHelp />
     </SectionContext.Provider>
   );
 }
