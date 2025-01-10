e8f762a9582b03d4c792961c33f46bd135240a62
cbum-dev <priyanshukumar2002234@gmail.com>
2025-01-10 22:32:17 +0530

    blog page fix

--- pages/blog/index.page.tsx
+++ pages/blog/index.page.tsx
@@ -92,36 +92,17 @@ export default function StaticMarkdownPage({
     setCurrentFilterTag(filterTag);
   }, [filterTag]);
 
-  const handleClick = (event: { currentTarget: { value: any } }) => {
-    const clickedTag = event.currentTarget.value;
-    setParam('type', clickedTag);
-
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault(); // Prevent default scrolling behavior
+    const clickedTag = event.currentTarget.value as blogCategories;
     if (clickedTag === 'All') {
-      setParam('type', null);
-    } else {
+      setCurrentFilterTag('All');
+      history.replaceState(null, '', '/blog'); // Update the URL without causing a scroll
+    } else if (isValidCategory(clickedTag)) {
       setCurrentFilterTag(clickedTag);
-    }
-
-    // Check if the user is already on the "/blog" page
-    if (router.pathname === '/blog') {
-      if (router.query.type) {
-        // Remove the 'type' query parameter from the URL
-        setParam(
-          'type',
-          router.query.type === clickedTag ? undefined : clickedTag,
-        );
-      }
-      setCurrentFilterTag(clickedTag);
-    } else {
-      // If not on the "/blog" page, navigate to the "/blog" page with the type tag as a query parameter
-      router.replace(
-        { pathname: '/blog', query: { type: clickedTag } },
-        undefined,
-        { shallow: true },
-      );
+      history.replaceState(null, '', `/blog?type=${clickedTag}`); // Update URL
     }
   };
-
   const recentBlog = blogPosts.sort((a, b) => {
     const dateA = new Date(a.frontmatter.date).getTime();
     const dateB = new Date(b.frontmatter.date).getTime();
