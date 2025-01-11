12b2dfabee0fcf2b8d3d2c39fc0f93c7de491cd4
techmannih <manishchaudharyttt@gmail.com>
2025-01-11 10:24:27 +0530

    fix sidebar height

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -234,7 +234,7 @@ export const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
           </div>
         </div>
         <div className='dark:bg-slate-800 max-w-[1400px] grid grid-cols-1 lg:grid-cols-4 mx-4 md:mx-12'>
-          <div className='hidden lg:block mt-24 mb-72 sticky top-24 h-[calc(100vh-6rem)] overflow-hidden'>
+          <div className='hidden lg:block mt-24 sticky top-24 h-[calc(100vh-6rem)] overflow-hidden'>
             <div className='h-full overflow-y-auto scrollbar-hidden'>
               <DocsNav open={open} setOpen={setOpen} />
               <CarbonAds
