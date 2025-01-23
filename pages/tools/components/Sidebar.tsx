16410114ee46f2449b28edd678e7b27da29b6170
Shubham Bhat <bhatsupshubham@gmail.com>
2025-01-23 13:32:46 +0530

    Revert "added dynamic icons to the filter in tools page"
    This reverts commit 8b5c6527a78d20e4cde4a00ecd70bfe780be2ab6.

--- pages/tools/components/Sidebar.tsx
+++ pages/tools/components/Sidebar.tsx
@@ -7,6 +7,7 @@ import LicenseIcon from '~/public/icons/license.svg';
 import DropdownMenu from './ui/DropdownMenu';
 import Checkbox from './ui/Checkbox';
 import SearchBar from './SearchBar';
+import FilterIcon from '~/public/icons/filter.svg';
 import toTitleCase from '../lib/toTitleCase';
 import type { Transform } from '../hooks/useToolsTransform';
 import type { FilterCriteriaFields } from '../index.page';
@@ -58,19 +59,11 @@ export default function Sidebar({
         groupBy: prev.groupBy || 'toolingTypes',
         languages: formData.getAll('languages').map((value) => value as string),
         licenses: formData.getAll('licenses').map((value) => value as string),
-        drafts: formData
-          .getAll('drafts')
-          .map((value) => value) as Transform['drafts'],
-        toolingTypes: formData
-          .getAll('toolingTypes')
-          .map((value) => value as string),
-        environments: formData
-          .getAll('environments')
-          .map((value) => value as string),
+        drafts: formData.getAll('drafts').map((value) => value) as Transform['drafts'],
+        toolingTypes: formData.getAll('toolingTypes').map((value) => value as string),
+        environments: formData.getAll('environments').map((value) => value as string),
         showObsolete:
-          (formData.get('showObsolete') as string) === 'showObsolete'
-            ? 'true'
-            : 'false',
+          (formData.get('showObsolete') as string) === 'showObsolete' ? 'true' : 'false',
       } satisfies Transform;
       postAnalytics({ eventType: 'query', eventPayload: newTransform });
       return newTransform;
@@ -85,6 +78,7 @@ export default function Sidebar({
     resetTransform();
     setIsSidebarOpen((prev) => !prev);
   };
+  const { resolvedTheme } = useTheme();
 
   return (
     <div className='pb-4 top-12 mx-auto lg:ml-4 lg:mt-8 w-4/5 h-fit'>
@@ -92,8 +86,7 @@ export default function Sidebar({
         <SearchBar transform={transform} />
         {filters.map(({ label, accessorKey }) => {
           const checkedValues = transform[accessorKey as keyof Transform] || [];
-          const IconComponent =
-            filterIcons[accessorKey as keyof typeof filterIcons];
+          const IconComponent = filterIcons[accessorKey as keyof typeof filterIcons]; 
           return (
             <DropdownMenu
               key={accessorKey}
@@ -130,6 +123,7 @@ export default function Sidebar({
             type='submit'
             className='bg-primary text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none'
           >
+            {/* <FilterIcon className="mr-0 w-4 h-4" /> */}
             Apply Filters
           </button>
           <button
