3c7b75d5a9276f763894b6d66cac8c0bb9bc425b
Idan Levi <29idan29@gmail.com>
2025-06-02 20:05:59 +0300

    mobile adjustment

--- pages/tools/index.page.tsx
+++ pages/tools/index.page.tsx
@@ -142,11 +142,22 @@ export default function ToolingPage({
 
         <div className='grid grid-cols-1 lg:grid-cols-4 mx-4 md:mx-12 min-h-screen'>
           <div
-            className={`lg:fixed absolute top-0 lg:top-0 left-0 lg:left-auto mt-36 lg:mt-20 w-screen lg:w-auto bg-white dark:bg-slate-800 lg:bg-transparent transition-transform lg:transform-none duration-300 lg:duration-0 ease-in-out z-5 ${isSidebarOpen ? '-translate-x-0' : '-translate-x-full'}`}
+            className={`
+    lg:fixed absolute top-0 lg:top-0 left-0 lg:left-auto
+    mt-36 lg:mt-20
+    w-screen lg:w-auto
+    bg-white dark:bg-slate-800 lg:bg-transparent
+    transition-transform lg:transform-none duration-300 lg:duration-0 ease-in-out
+    z-5
+    ${isSidebarOpen ? '-translate-x-0' : '-translate-x-full'}
+    overflow-y-auto lg:overflow-y-hidden
+  `}
             style={{
-              height: 'calc(115vh - 10rem)',
-              maxHeight: 'calc(115vh - 10rem)',
-              bottom: '0',
+              height: 'calc(100vh - 4.5rem)',
+              maxHeight: 'calc(100vh - 4.5rem)',
+              bottom: 0,
+              /* remove inline overflowY here */
+              scrollbarWidth: 'none',
             }}
           >
             <div className='h-full flex flex-col'>
