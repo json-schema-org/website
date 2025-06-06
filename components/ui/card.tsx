87a1e620477968d86e1511aeff21fc10fc4d1d8b
Idan Levi <29idan29@gmail.com>
2025-06-06 16:35:26 +0300

    fixing prop type bug

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,10 +1,15 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable quotes */
+/* eslint-disable react/prop-types */
 import * as React from 'react';
+import PropTypes from 'prop-types';
 
 import { cn } from '@/lib/utils';
 
-function Card({ className, ...props }: React.ComponentProps<'div'>) {
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+function Card({ className, ...props }: CardProps) {
   return (
     <div
       data-slot='card'
@@ -17,7 +22,15 @@ function Card({ className, ...props }: React.ComponentProps<'div'>) {
   );
 }
 
-function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
+Card.propTypes = {
+  className: PropTypes.string,
+};
+
+interface CardHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+function CardHeader({ className, ...props }: CardHeaderProps) {
   return (
     <div
       data-slot='card-header'
@@ -30,7 +43,15 @@ function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
   );
 }
 
-function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
+CardHeader.propTypes = {
+  className: PropTypes.string,
+};
+
+interface CardTitleProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+function CardTitle({ className, ...props }: CardTitleProps) {
   return (
     <div
       data-slot='card-title'
@@ -40,7 +61,15 @@ function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
   );
 }
 
-function CardDescription({ className, ...props }: React.ComponentProps<'div'>) {
+CardTitle.propTypes = {
+  className: PropTypes.string,
+};
+
+interface CardDescriptionProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+function CardDescription({ className, ...props }: CardDescriptionProps) {
   return (
     <div
       data-slot='card-description'
@@ -50,7 +79,15 @@ function CardDescription({ className, ...props }: React.ComponentProps<'div'>) {
   );
 }
 
-function CardAction({ className, ...props }: React.ComponentProps<'div'>) {
+CardDescription.propTypes = {
+  className: PropTypes.string,
+};
+
+interface CardActionProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+function CardAction({ className, ...props }: CardActionProps) {
   return (
     <div
       data-slot='card-action'
@@ -63,7 +100,15 @@ function CardAction({ className, ...props }: React.ComponentProps<'div'>) {
   );
 }
 
-function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
+CardAction.propTypes = {
+  className: PropTypes.string,
+};
+
+interface CardContentProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+function CardContent({ className, ...props }: CardContentProps) {
   return (
     <div
       data-slot='card-content'
@@ -73,7 +118,15 @@ function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
   );
 }
 
-function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
+CardContent.propTypes = {
+  className: PropTypes.string,
+};
+
+interface CardFooterProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+function CardFooter({ className, ...props }: CardFooterProps) {
   return (
     <div
       data-slot='card-footer'
@@ -82,6 +135,10 @@ function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
     />
   );
 }
+
+CardFooter.propTypes = {
+  className: PropTypes.string,
+};
 
 export {
   Card,
