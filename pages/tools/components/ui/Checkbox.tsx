717d30cc63fb430f114563d80e0c2ff4a5955d70
Idan Levi <127854883+idanidan29@users.noreply.github.com>
2025-06-14 09:46:44 +0300

    Adapt Checkbox & SearchBar to ShadCN, Add Tests, and Fix Tools Filter position (#1686)
    * Implementing Checkbox.tsx
    
    * simplifying and containing Checkbox.tsx
    
    * implementing SearchBar.tsx
    
    * adjusting border color
    
    * adding cypress test file for checkbox
    
    * added a test file for search bar
    
    * fixing issue #1279
    
    * fixing spacing
    
    * spacing adjustments
    
    * mobile adjustment
    
    * last spacing adjustments
    
    * format adjustments
    
    * adding prop types for format fixing
    
    * adjusting the cypress test for searchbar
    
    * small format fix
    
    * keeping original width

--- pages/tools/components/ui/Checkbox.tsx
+++ pages/tools/components/ui/Checkbox.tsx
@@ -1,34 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Checkbox as ShadcnCheckbox } from '@/components/ui/checkbox';
 
 export default function Checkbox({
   label,
   value,
   name,
   checked,
+  disabled,
 }: {
   label: string;
   value: string;
   name: string;
   checked?: boolean;
+  disabled?: boolean;
 }) {
-  const [isChecked, setIsChecked] = useState(checked);
-
-  useEffect(() => {
-    setIsChecked(checked);
-  }, [checked]);
-
-  const handleChange = () => {
-    setIsChecked((prevChecked) => !prevChecked);
-  };
-
   return (
-    <label className='flex items-center gap-3 px-4 py-2 cursor-pointer'>
-      <input
-        type='checkbox'
+    <label className='flex items-center gap-3 px-4 py-2 cursor-pointer bg-slate-200 dark:bg-slate-900 hover:bg-slate-300 dark:hover:bg-slate-800 transition-colors duration-200 border border-slate-300 dark:border-slate-700 rounded-md my-2'>
+      <ShadcnCheckbox
         value={value}
         name={name}
-        checked={isChecked}
-        onChange={handleChange}
+        defaultChecked={checked}
+        disabled={disabled}
+        className='h-5 w-5 border data-[state=checked]:bg-blue-500 data-[state=checked]:border-blue-500 data-[state=checked]:text-white dark:data-[state=checked]:bg-[#bfdbfe] dark:data-[state=checked]:border-[#bfdbfe] dark:data-[state=checked]:text-black border-gray-500 dark:border-slate-600'
       />
       <span className='text-gray-700 dark:text-slate-300 font-medium'>
         {label}
