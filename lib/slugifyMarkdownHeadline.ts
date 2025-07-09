5b2149fa55a6dbc576815333a934432430c816b2
Aqib Nawab <aqibnawab1100@gmail.com>
2025-07-09 09:56:43 +0500

    Add Navigation into the japnese

--- lib/slugifyMarkdownHeadline.ts
+++ lib/slugifyMarkdownHeadline.ts
@@ -4,23 +4,49 @@ export default function slugifyMarkdownHeadline(
   markdownChildren: string | any[],
 ): string {
   const FRAGMENT_REGEX = /\[#(?<slug>(\w|-|_)*)\]/g;
+
   if (!markdownChildren) return '';
+
   if (typeof markdownChildren === 'string')
-    return slugify(markdownChildren, { lower: true, trim: true });
+    return slugify(markdownChildren, {
+      lower: true,
+
+      trim: true,
+
+      remove: /[*+~()'"!]/g,
+    });
+
   const metaSlug = markdownChildren.reduce((acc, child) => {
     if (acc) return acc;
+
     if (typeof child !== 'string') return null;
+
     const fragment = FRAGMENT_REGEX.exec(child);
+
     if (!fragment) return null;
+
     const slug = fragment?.groups?.slug;
+
     return slug || null;
   }, null);
+
   if (metaSlug) return metaSlug;
 
   const joinedChildren = markdownChildren
+
     .filter((child) => typeof child === 'string')
+
     .map((string) => string.replace(FRAGMENT_REGEX, ''))
+
     .join(' ');
-  const slug = slugify(joinedChildren, { lower: true, trim: true });
+
+  const slug = slugify(joinedChildren, {
+    lower: true,
+
+    trim: true,
+
+    remove: /[*+~()'"!]/g,
+  });
+
   return slug;
 }
