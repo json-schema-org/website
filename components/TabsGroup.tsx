8accfacd4012e2046ee94d76ec51638f9e41f688
Idan Levi <29idan29@gmail.com>
2025-08-01 18:28:04 +0300

    adjusting tabs for mobile

--- components/TabsGroup.tsx
+++ components/TabsGroup.tsx
@@ -19,11 +19,11 @@ export const TabsGroup = ({
 
   return (
     <div>
-      <div className='flex flex-row items-end mt-4'>
+      <div className='flex flex-col sm:flex-row items-start sm:items-end mt-4'>
         {groupLabel && (
-          <div className='p-4 text-slate-400 mr-4 text-sm'>{groupLabel}:</div>
+          <div className='p-2 sm:p-4 text-slate-400 mb-2 sm:mb-0 sm:mr-4 text-sm'>{groupLabel}:</div>
         )}
-        <div className='flex flex-row'>
+        <div className='flex flex-row overflow-x-auto w-full sm:w-auto'>
           {tabs.map((tab, index) => {
             const isActive = index === activeTabIndex;
             return (
@@ -31,7 +31,7 @@ export const TabsGroup = ({
                 key={index}
                 onClick={() => setActiveTabIndex(index)}
                 className={classnames(
-                  'p-4 px-6 text-slate-700 font-medium border-b-2 rounded-t-lg',
+                  'p-2 sm:p-4 px-3 sm:px-6 text-slate-700 font-medium border-b-2 rounded-t-lg whitespace-nowrap text-sm sm:text-base',
                   {
                     'border-blue-400 text-blue-500 bg-blue-50 dark:bg-slate-900 dark:text-white':
                       isActive,
@@ -46,7 +46,7 @@ export const TabsGroup = ({
           })}
         </div>
       </div>
-      <div className='border-slate-100 mb-4 p-6 from-slate-50/50 to-slate-50/100 rounded-xl bg-gradient-to-b dark:from-slate-700/50 dark:to-slate-900/50'>
+      <div className='border-slate-100 mb-4 p-3 sm:p-6 from-slate-50/50 to-slate-50/100 rounded-xl bg-gradient-to-b dark:from-slate-700/50 dark:to-slate-900/50'>
         <StyledMarkdownBlock markdown={activeTab.markdown} />
       </div>
     </div>
