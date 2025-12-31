44901555a9a192a2a2e4202fa878a9c9d1befd31
Sam <samailahmad61@gmail.com>
2025-12-31 09:03:16 +0000

    chore: fix prettier formatting in jsonEditor.tsx

--- components/JsonEditor.tsx
+++ components/JsonEditor.tsx
@@ -308,7 +308,9 @@ export default function JsonEditor({
     if (!positionOfCodeInFullMarkdown) return;
     const endPositionOfCode = positionOfCodeInFullMarkdown + codeContent.length;
     const startPositionOfNextBlock = endPositionOfCode + '\n```\n'.length;
-    const markdownAfterCodeBlock = fullMarkdown?.slice(startPositionOfNextBlock);
+    const markdownAfterCodeBlock = fullMarkdown?.slice(
+      startPositionOfNextBlock,
+    );
     return markdownAfterCodeBlock?.startsWith('```');
   })();
 
