ad48d425fc1b1e3aa2b958cc656d749dc336f8fd
Surajiitmjnu <25f2005650@ds.study.iitm.ac.in>
2026-02-11 12:12:15 +0530

    fix: resolve jsx quote lint issues

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1002,7 +1002,11 @@ for Accounting integrations'
             </p>
           </div>
           <div className='flex flex-col items-center md:flex-row justify-center text-center gap-x-14 gap-y-4'>
-            <a href='https://www.commonroom.io' target='_blank'  rel="noopener noreferrer">
+            <a
+              href='https://www.commonroom.io'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               {isClient && (
                 <>
                   <Image
@@ -1015,7 +1019,11 @@ for Accounting integrations'
                 </>
               )}
             </a>
-            <a href='https://json-schema.org/slack' target='_blank' rel="noopener noreferrer">
+            <a
+              href='https://json-schema.org/slack'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               {isClient && (
                 <>
                   <Image
