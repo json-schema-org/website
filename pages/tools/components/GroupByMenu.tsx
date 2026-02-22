1f561aac3320655fa647c102650199bfe24a0a0c
Anuj Agarwal <anuj@Anujs-MacBook-Pro.local>
2026-02-22 19:50:50 +0530

    Updated button position for bigger screens and arrow down image

--- pages/tools/components/GroupByMenu.tsx
+++ pages/tools/components/GroupByMenu.tsx
@@ -50,7 +50,7 @@ const GroupByMenu = ({
   };
 
   return (
-    <div className='ml-2 my-8 flex flex-col gap-y-4 w-full'>
+    <div className='ml-2 my-8 flex flex-col lg:flex-row items-start lg:items-center gap-4 w-full'>
       <div className='flex items-center space-x-2 max-w-screen overflow-x-auto'>
         <span className='text-slate-600 dark:text-slate-300'>GROUP BY:</span>
         {groupBy.map((group) => {
@@ -68,7 +68,7 @@ const GroupByMenu = ({
         })}
       </div>
       {activeSections.length > 0 && groupedBy !== 'none' && (
-        <div className='relative inline-block w-48'>
+        <div className='relative inline-block w-48 lg:ml-2'>
           <select
             onChange={(e) => scrollToSection(e.target.value)}
             defaultValue=''
@@ -83,8 +83,10 @@ const GroupByMenu = ({
               </option>
             ))}
           </select>
-          <div className='pointer-events-none absolute inset-y-0 right-3 flex items-center text-xs text-slate-500'>
-            ▼
+          <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-slate-500'>
+            <svg className='h-4 w-4 fill-current' viewBox='0 0 20 20'>
+              <path d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z' />
+            </svg>
           </div>
         </div>
       )}
