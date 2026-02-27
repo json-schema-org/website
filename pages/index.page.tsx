d78b95996d0879e6f5e91294e6dcc0473a1de26c
Surajiitmjnu <25f2005650@ds.study.iitm.ac.in>
2026-02-27 22:38:13 +0530

    Fix lint and formatting issues

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -672,7 +672,10 @@ const Home = (props: any) => {
               </svg>
               <p>Your logo here</p>
             </Link>
-            <h3 className='p-4 text-h4mobile md:text-h4 font-semibold my-4 dark:text-slate-200'  id="bronze-sponsors"   >
+            <h3
+              className='p-4 text-h4mobile md:text-h4 font-semibold my-4 dark:text-slate-200'
+              id='bronze-sponsors'
+            >
               Bronze Sponsors
             </h3>
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-12 items-center mx-auto  md:mx-0 px-4 '>
