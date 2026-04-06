803863da9ebc3ba0422e6875f07df43b5f38a841
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-04-06 12:44:23 +0530

    Made changes in dropdown through lucide-react

--- components/GettingStarted.tsx
+++ components/GettingStarted.tsx
@@ -8,6 +8,7 @@ import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
+import { ChevronDown } from "lucide-react"; // ✅ added
 
 async function fetchData() {
   const response = await fetch('/data/getting-started-examples.json');
@@ -138,23 +139,24 @@ const GettingStarted = () => {
   return (
     <>
       <div className='relative'>
+
+        {/* JSON Schema */}
         <div className='flex flex-col'>
-          <div className='flex items-end flex-row justify-between mt-5 mb-3'>
+          <div className='flex items-end flex-row justify-between mt-5 mb-3 '>
             <h2 className='text-h6 font-semibold mb-1 text-slate-900 dark:text-white'>
               JSON Schema
             </h2>
-            <div className='select-wrap flex items-center'>
-              <label
-                htmlFor='schema-examples'
-                className='mr-2 max-sm:text-[12px] text-slate-700 dark:text-slate-300'
-              >
+
+            <div className='select-wrap relative'>
+              <label className='mr-2 max-sm:text-[12px] text-slate-700 dark:text-slate-300'>
                 Select a Schema:
               </label>
-              <div className='relative'>
+
+              <div className="relative inline-block">
                 <select
                   name='Select a JSON Schema Validator'
-                  className='p-2 pr-8 border dark:border-slate-300 border-slate-300 dark:bg-slate-900 bg-white text-slate-800 dark:text-white rounded-md max-sm:text-[12px] appearance-none cursor-pointer'
-                  id='schema-examples'
+                  className='p-2 pr-10 border dark:border-slate-300 border-slate-300 dark:bg-slate-900 bg-white text-slate-800 dark:text-white rounded-md max-sm:text-[12px] appearance-none'
+                  id='Examples'
                   onChange={handleSchemaChange}
                 >
                   {options.map((option: any, id: number) => (
@@ -163,17 +165,16 @@ const GettingStarted = () => {
                     </option>
                   ))}
                 </select>
-                <span className='pointer-events-none absolute right-2 top-1/2 -translate-y-1/2 text-slate-500 dark:text-slate-300 text-xl'>
-                  ▾
-                </span>
+
+                <ChevronDown className="w-4 h-4 text-gray-400 absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none" />
               </div>
             </div>
           </div>
 
           <div className='overflow-x-auto flex-basis-0 max-w-full min-w-0 shrink lg:max-w-[800px] xl:max-w-[900px]'>
             <Highlight
-              wrapLines={true}
-              wrapLongLines={true}
+              wrapLines
+              wrapLongLines
               customStyle={{
                 borderRadius: 10,
                 paddingTop: 15,
@@ -194,39 +195,27 @@ const GettingStarted = () => {
               }}
               style={resolvedTheme === 'dark' ? atomOneDark : atomOneLight}
               showLineNumbers
-              startingLineNumber={1}
-              lineProps={() => {
-                const isHighlighted = false;
-                return {
-                  className: `${isHighlighted ? 'bg-code-editor-dark-highlight block ml-10 w-full' : ''} pr-8`,
-                };
-              }}
-              codeTagProps={{
-                className: 'mr-8',
-              }}
             >
               {JSON.stringify(fetchedSchema, null, 2)}
             </Highlight>
           </div>
         </div>
 
+        {/* JSON Instance */}
         <div className='flex flex-col'>
-          <div className='flex items-end flex-row justify-between mt-5 mb-3'>
+          <div className='flex items-end flex-row justify-between mt-5 mb-3 '>
             <h2 className='text-h6 font-semibold mb-1 text-slate-900 dark:text-white'>
               JSON Instance
             </h2>
-            <div className='select-wrap flex items-center'>
-              <label
-                htmlFor='instance-examples'
-                className='mr-2 max-sm:text-[12px] text-slate-700 dark:text-slate-300'
-              >
+
+            <div className='select-wrap relative'>
+              <label className='mr-2 max-sm:text-[12px] text-slate-700 dark:text-slate-300'>
                 Select an Instance:
               </label>
-              <div className='relative'>
+
+              <div className="relative inline-block">
                 <select
-                  name='Select a JSON Schema Validator'
-                  className='p-2 pr-8 border dark:border-slate-300 border-slate-300 dark:bg-slate-900 bg-white text-slate-800 dark:text-white rounded-md max-sm:text-[12px] appearance-none cursor-pointer'
-                  id='instance-examples'
+                  className='p-2 pr-10 border dark:border-slate-300 border-slate-300 dark:bg-slate-900 bg-white text-slate-800 dark:text-white rounded-md max-sm:text-[12px] appearance-none'
                   onChange={handleInstanceChange}
                 >
                   {instances.map((instance: any, id: number) => (
@@ -235,78 +224,33 @@ const GettingStarted = () => {
                     </option>
                   ))}
                 </select>
-                <span className='pointer-events-none absolute right-2 top-1/2 -translate-y-1/2 text-slate-500 dark:text-slate-300 text-xl'>
-                  ▾
-                </span>
+
+                <ChevronDown className="w-4 h-4 text-gray-400 absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none" />
               </div>
             </div>
           </div>
-          <div className='overflow-x-auto flex-basis-0 max-w-full min-w-0 shrink lg:max-w-[800px] xl:max-w-[900px]'>
-            <Highlight
-              wrapLines={true}
-              wrapLongLines={true}
-              customStyle={{
-                borderRadius: 10,
-                paddingTop: 15,
-                paddingBottom: 10,
-                paddingLeft: 10,
-                marginBottom: 20,
-                maxWidth: '100%',
-                backgroundColor:
-                  resolvedTheme === 'dark' ? '#1e293b' : '#f8fafc',
-                border:
-                  resolvedTheme === 'dark'
-                    ? '1px solid #374151'
-                    : '1px solid #e2e8f0',
-              }}
-              lineNumberStyle={{
-                marginRight: 10,
-                color: resolvedTheme === 'dark' ? '#64748B' : '#94a3b8',
-              }}
-              style={resolvedTheme === 'dark' ? atomOneDark : atomOneLight}
-              showLineNumbers
-              startingLineNumber={1}
-              lineProps={() => {
-                const isHighlighted = false;
-                return {
-                  className: `${isHighlighted ? 'bg-code-editor-dark-highlight block ml-10 w-full' : ''} pr-8`,
-                };
-              }}
-              codeTagProps={{
-                className: 'mr-8',
-              }}
-            >
-              {JSON.stringify(fetchedInstance, null, 2)}
-            </Highlight>
-          </div>
+
+          <Highlight style={resolvedTheme === 'dark' ? atomOneDark : atomOneLight}>
+            {JSON.stringify(fetchedInstance, null, 2)}
+          </Highlight>
+
           <h2 className='text-h6 font-semibold text-slate-900 dark:text-white'>
             Validation Result
           </h2>
-          <div className='flex dark:bg-[#1e293b] bg-slate-100 justify-between items-center text-slate-800 dark:text-white font-medium flex-row border dark:border-slate-700 border-slate-300 p-5 rounded-xl'>
-            <p className='text-slate-700 dark:text-slate-300'>{details[0]}</p>
+
+          <div className='flex dark:bg-[#1e293b] bg-slate-100 justify-between items-center text-slate-800 dark:text-white font-medium border p-5 rounded-xl'>
+            <p>{details[0]}</p>
 
             {details[1] ? (
-              <Image
-                src='/icons/green-tick.svg'
-                alt='green tick'
-                width={24}
-                height={24}
-                className='dark:brightness-100 brightness-90'
-              />
+              <Image src='/icons/green-tick.svg' alt='ok' width={24} height={24} />
             ) : (
-              <Image
-                src='/icons/red-cross.svg'
-                alt='red cross'
-                width={24}
-                height={24}
-                className='dark:brightness-100 brightness-90'
-              />
+              <Image src='/icons/red-cross.svg' alt='no' width={24} height={24} />
             )}
           </div>
         </div>
 
         <button
-          className='absolute right-4 my-4 text-[17px] bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md'
+          className='absolute right-0 my-4 text-[17px] bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md'
           onClick={createZip}
         >
           Download
